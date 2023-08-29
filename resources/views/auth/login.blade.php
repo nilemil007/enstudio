4bb97bd2fa1f7eafb3ed0c941c6648ef9c02d457
Emil Sadekin Islam <nilemil007@gmail.com>
2023-08-29 21:24:01 +0600

    Modified login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,39 +2,35 @@
 
     <x-slot:title>Login</x-slot:title>
 
-    <div class="row w-100 mx-0 auth-page">
-        <div class="col-md-4 mx-auto">
-            <div class="card">
-                <div class="row">
-                    <div class="col-md-12 ps-md-0">
-                        <div class="auth-form-wrapper px-4 py-5">
-                            <a href="#" class="noble-ui-logo logo-light d-block mb-2">EN<span>Studio</span></a>
-                            <h5 class="text-muted fw-normal mb-4">Welcome back! Log in to your account.</h5>
-                            <form id="login" class="forms-sample" method="POST" action="{{ route('login') }}">
-                                @csrf
+    <div class="row mx-0 auth-page">
+        <div class="card">
+            <div class="col-md-12 ps-md-0">
+                <div class="auth-form-wrapper px-4 py-5">
+                    <a href="{{ route('login') }}" class="noble-ui-logo logo-light d-block mb-2">EN<span>Studio</span></a>
+                    <h5 class="text-muted fw-normal mb-4">Welcome back! Log in to your account.</h5>
+                    <form id="login" class="forms-sample" method="POST" action="{{ route('login') }}">
+                        @csrf
 
-                                <div class="mb-3">
-                                    <label for="login" class="form-label">Login ID</label>
-                                    <input type="text" name="login" value="{{ old('login') }}" class="form-control @error('login') is-invalid @enderror" id="login" placeholder="Enter Username, Email or Phone">
-                                    @error('login') <small class="text-danger" style="font-weight: bold">{{ $message }}</small> @enderror
-                                </div>
-                                <div class="mb-3">
-                                    <label for="password" class="form-label">Password</label>
-                                    <input type="password" name="password" class="form-control @error('password') is-invalid @enderror" id="password" placeholder="Enter password">
-                                    @error('password') <small class="text-danger" style="font-weight: bold">{{ $message }}</small> @enderror
-                                </div>
-                                <div class="form-check mb-3">
-                                    <input class="form-check-input" name="remember" type="checkbox" id="remember">
-                                    <label class="form-check-label" for="remember">
-                                        Remember me
-                                    </label>
-                                </div>
-                                <div>
-                                    <button type="submit" class="btn btn-primary me-2 mb-2 mb-md-0 text-white w-100">Login</button>
-                                </div>
-                            </form>
+                        <div class="mb-3">
+                            <label for="login" class="form-label">Login ID</label>
+                            <input type="text" name="login" value="{{ old('login') }}" class="form-control @error('login') is-invalid @enderror" id="login" placeholder="Enter Username, Email or Phone">
+                            @error('login') <small class="text-danger" style="font-weight: bold">{{ $message }}</small> @enderror
                         </div>
-                    </div>
+                        <div class="mb-3">
+                            <label for="password" class="form-label">Password</label>
+                            <input type="password" name="password" class="form-control @error('password') is-invalid @enderror" id="password" placeholder="Enter password">
+                            @error('password') <small class="text-danger" style="font-weight: bold">{{ $message }}</small> @enderror
+                        </div>
+                        <div class="form-check mb-3">
+                            <input class="form-check-input" name="remember" type="checkbox" id="remember">
+                            <label class="form-check-label" for="remember">
+                                Remember me
+                            </label>
+                        </div>
+                        <div>
+                            <button type="submit" class="btn btn-primary me-2 mb-2 mb-md-0 text-white w-100">Login</button>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('auth/login');
-});
+})->name('login');
 
 
 Route::middleware(['auth'])->group(function (){
